32cb267749311c7ef1adb083486528cdcca9fc55
Teddy Regorgo Dumagat <tedzzzregor@gmail.com>
2021-02-10 15:55:06 +0800

    Updates

---
+++ precache-manifest.2657f1b24e6ff79744ac0e6873489315.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5af488987694c50af7e4f1c2893574f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a76dcf743398c468ea79",
+    "url": "/portfolio/static/css/main.1abaef04.chunk.css"
+  },
+  {
+    "revision": "482253f1b525d4c47a99",
+    "url": "/portfolio/static/js/2.718a1023.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.718a1023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a76dcf743398c468ea79",
+    "url": "/portfolio/static/js/main.b0cb1a87.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "fcfdb31d3da50a41367e84f38d5f190a",
+    "url": "/portfolio/static/media/Resume.fcfdb31d.pdf"
+  },
+  {
+    "revision": "30842f99bb2358f93aa1b6c014922aef",
+    "url": "/portfolio/static/media/cert.30842f99.png"
+  },
+  {
+    "revision": "9c2bc7f8ba9c856a6f605b926585d2b6",
+    "url": "/portfolio/static/media/coffee.9c2bc7f8.jpg"
+  },
+  {
+    "revision": "0fb30b49da79da6979871081a23bfd53",
+    "url": "/portfolio/static/media/expressjs.0fb30b49.svg"
+  },
+  {
+    "revision": "827a1ef7ef0b87370f47ed5e2658b26e",
+    "url": "/portfolio/static/media/facerecognition.827a1ef7.png"
+  },
+  {
+    "revision": "448ccda7e4e25ed738f8ea6f0a253cfb",
+    "url": "/portfolio/static/media/laptop.448ccda7.jpg"
+  },
+  {
+    "revision": "69d21a91d1994982e4b29fd81a09d771",
+    "url": "/portfolio/static/media/my_icafe.69d21a91.jpg"
+  },
+  {
+    "revision": "da26917b64ee76074c9910767889bdb5",
+    "url": "/portfolio/static/media/my_image.da26917b.jpg"
+  },
+  {
+    "revision": "2f43116572b62d4d438bb7556162acf0",
+    "url": "/portfolio/static/media/portfolio.2f431165.png"
+  },
+  {
+    "revision": "601b2e5664e64192e6e7c439a961e80b",
+    "url": "/portfolio/static/media/reekoh.601b2e56.jpg"
+  },
+  {
+    "revision": "600c3457271dd7689e07788fe467275f",
+    "url": "/portfolio/static/media/robofriends.600c3457.png"
+  },
+  {
+    "revision": "442cf57dbe6df489c853d82a4693d774",
+    "url": "/portfolio/static/media/sit_dinner.442cf57d.jpg"
+  },
+  {
+    "revision": "d659fdb4bfa65a38ff6dc724d9e78a8a",
+    "url": "/portfolio/static/media/sit_family.d659fdb4.jpg"
+  },
+  {
+    "revision": "1f39375913197c265623a2713d65a5cd",
+    "url": "/portfolio/static/media/sit_id.1f393759.jpg"
+  },
+  {
+    "revision": "0df3baa36a21b273afd9c670cec2b04b",
+    "url": "/portfolio/static/media/works.0df3baa3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.d9f8740fc96bf80aa68690f4b1352899.js"
+  "/portfolio/precache-manifest.2657f1b24e6ff79744ac0e6873489315.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
