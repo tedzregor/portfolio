be5c26f2004ad890925099105caeda246ab78a71
Teddy Regorgo Dumagat <tedzzzregor@gmail.com>
2021-02-10 15:48:16 +0800

    Updates

---
+++ precache-manifest.d9f8740fc96bf80aa68690f4b1352899.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e75dee61c2063cf860249000e69911c",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "0e342faf86ba53fe02b0",
+    "url": "/my-portfolio/static/css/main.0ee26905.chunk.css"
+  },
+  {
+    "revision": "482253f1b525d4c47a99",
+    "url": "/my-portfolio/static/js/2.718a1023.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.718a1023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e342faf86ba53fe02b0",
+    "url": "/my-portfolio/static/js/main.9a68286e.chunk.js"
+  },
+  {
+    "revision": "feb14810df33d645b05c",
+    "url": "/my-portfolio/static/js/runtime-main.9efc66b9.js"
+  },
+  {
+    "revision": "fcfdb31d3da50a41367e84f38d5f190a",
+    "url": "/my-portfolio/static/media/Resume.fcfdb31d.pdf"
+  },
+  {
+    "revision": "30842f99bb2358f93aa1b6c014922aef",
+    "url": "/my-portfolio/static/media/cert.30842f99.png"
+  },
+  {
+    "revision": "9c2bc7f8ba9c856a6f605b926585d2b6",
+    "url": "/my-portfolio/static/media/coffee.9c2bc7f8.jpg"
+  },
+  {
+    "revision": "0fb30b49da79da6979871081a23bfd53",
+    "url": "/my-portfolio/static/media/expressjs.0fb30b49.svg"
+  },
+  {
+    "revision": "827a1ef7ef0b87370f47ed5e2658b26e",
+    "url": "/my-portfolio/static/media/facerecognition.827a1ef7.png"
+  },
+  {
+    "revision": "448ccda7e4e25ed738f8ea6f0a253cfb",
+    "url": "/my-portfolio/static/media/laptop.448ccda7.jpg"
+  },
+  {
+    "revision": "69d21a91d1994982e4b29fd81a09d771",
+    "url": "/my-portfolio/static/media/my_icafe.69d21a91.jpg"
+  },
+  {
+    "revision": "da26917b64ee76074c9910767889bdb5",
+    "url": "/my-portfolio/static/media/my_image.da26917b.jpg"
+  },
+  {
+    "revision": "2f43116572b62d4d438bb7556162acf0",
+    "url": "/my-portfolio/static/media/portfolio.2f431165.png"
+  },
+  {
+    "revision": "601b2e5664e64192e6e7c439a961e80b",
+    "url": "/my-portfolio/static/media/reekoh.601b2e56.jpg"
+  },
+  {
+    "revision": "600c3457271dd7689e07788fe467275f",
+    "url": "/my-portfolio/static/media/robofriends.600c3457.png"
+  },
+  {
+    "revision": "442cf57dbe6df489c853d82a4693d774",
+    "url": "/my-portfolio/static/media/sit_dinner.442cf57d.jpg"
+  },
+  {
+    "revision": "d659fdb4bfa65a38ff6dc724d9e78a8a",
+    "url": "/my-portfolio/static/media/sit_family.d659fdb4.jpg"
+  },
+  {
+    "revision": "1f39375913197c265623a2713d65a5cd",
+    "url": "/my-portfolio/static/media/sit_id.1f393759.jpg"
+  },
+  {
+    "revision": "0df3baa36a21b273afd9c670cec2b04b",
+    "url": "/my-portfolio/static/media/works.0df3baa3.jpg"
+  }
+]);
